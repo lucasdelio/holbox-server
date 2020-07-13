475e0f0098d558e0aa6a15fb4da3e5dcee05e653
Lucas De Lio <lucasdelio@gmail.com>
2020-07-12 23:52:20 -0300

    added categories and PUT method

--- holbox.py
+++ holbox.py
@@ -8,6 +8,11 @@ from cryptography.fernet import Fernet
 from bson.objectid import ObjectId
 import os
 
+MARKDOWN = "markdown"
+TITLE = "title"
+CATEGORY = "category"
+TAGS = "tags"
+
 GOOGLE_USER_INFO_URL = 'https://openidconnect.googleapis.com/v1/userinfo'
 JSON_HEADER = {'content-type':'application/json'}
 TTL_ = 24*60*60 #24 hs ttl for generated tokens
@@ -26,33 +31,80 @@ articles_collection = client.holbox_database.articles_collection
 def articles():
     p = articles_collection.find()
     return json_util.dumps(p), 200, JSON_HEADER
-    
-@app.route('/article', methods=['GET','PUT','DELETE'])
+
+#{
+#    "markdown":"este es el markdown 234",
+#    "title": "titulo",
+#    "category": "categoria", 
+#    "tags": [ "tag1", "tag2", "tag3"]
+#}
+def isValidArticle(a):
+    return a[MARKDOWN] and a[TITLE] and a[CATEGORY] and a[TAGS]
+
+@app.route('/article', methods=['GET','POST','DELETE','PUT'])
 def article_by_id():
     id = request.args.get('id')
-    if request.method == 'GET':
-        if not id: return 'no id param',400
-        print(id)
+    if request.method == 'GET':   
+        if not id: return 'no id param', 400
         a = articles_collection.find_one( {'_id': ObjectId(id)} )
         if not a :
-            return '',204 #204 no content
+            return '', 204 #204 no content
         return json_util.dumps(a), 200, JSON_HEADER
-    if request.method == 'PUT':
+    if request.method == 'POST':
         try:
-            body = request.data.decode('utf8')
-            articles_collection.insert_one( {
-                'markdown':body,
-                'date': str(datetime.utcnow())
-                })
-            return '',200
+            article = json.loads( request.data.decode('utf8')  )
+            if not isValidArticle(article):
+                return 'invalid article format', 400
+            article['date'] = str(datetime.utcnow())
+            articles_collection.insert_one( article )
+            return article, 200
         except:
-            return 'Invalid body',400
+            return 'Invalid body, body must be a valid article', 400
     if request.method == 'DELETE':
-        if not id: return 'no id param',400
+        if not id: return 'no id param', 400
         result = articles_collection.delete_one( {'_id': ObjectId(id)} )
         if result.deleted_count > 0:
-            return '',200
-        return '',204
+            return '', 200
+        return 'id not found', 204
+    if request.method == 'PUT':   
+        if not id:
+            return 'no id param', 400
+        article = json.loads( request.data.decode('utf8')  )
+        if not isValidArticle(article):
+            return 'invalid article format', 400
+        a = articles_collection.find_one( {'_id': ObjectId(id)} )
+        if not a:
+            return 'id not found', 204
+        article["date"] = a["date"] #preserve the old date
+        articles_collection.replace_one( {'_id': ObjectId(id)} , article)
+        return article, 200, JSON_HEADER
+
+
+@app.route('/news')
+def get_news():
+    p = articles_collection.find({"category": "news"})
+    return json_util.dumps(p), 200, JSON_HEADER
+
+@app.route('/tips')
+def get_tips():
+    p = articles_collection.find({"category": "tips"})
+    return json_util.dumps(p), 200, JSON_HEADER
+
+@app.route('/gallery')
+def get_gallery():
+    p = articles_collection.find({"category": "gallery"})
+    return json_util.dumps(p), 200, JSON_HEADER
+
+@app.route('/destinies')
+def get_destinies():
+    p = articles_collection.find({"category": "destinies"})
+    return json_util.dumps(p), 200, JSON_HEADER
+
+@app.route('/experiences')
+def get_experiences():
+    p = articles_collection.find({"category": "experiences"})
+    return json_util.dumps(p), 200, JSON_HEADER
+
 
 if __name__ == '__main__':
     app.run(debug=True)
